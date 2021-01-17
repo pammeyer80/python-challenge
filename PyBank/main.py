8741aa6d45a415d06e6d7abc29c11ee75b1c79d6
Pam M <22107270+pammeyer80@users.noreply.github.com>
2021-01-16 20:01:05 -0600

    update PyBank main.py with average change analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,8 @@ budget_data_csv = os.path.join('Resources', 'budget_data.csv')
 
 counter = 0
 total = 0
+profit_change = 0
+profit_change_total = 0
 average_change = 0
 greatest_increase = 0
 greatest_decrease = 0
@@ -25,10 +27,15 @@ with open(budget_data_csv, 'r') as csvfile:
 
         month = str(row[0])
         current_value = int(row[1])
-        profit_change = current_value - previous_value
 
         counter += 1
-        total += int(current_value)
+        total += current_value
+        
+        if counter == 1:
+            starting_value = current_value
+        elif counter > 1:
+            profit_change = current_value - previous_value
+            profit_change_total += profit_change
 
         if profit_change >= greatest_increase:
             greatest_increase_month = month
@@ -39,12 +46,15 @@ with open(budget_data_csv, 'r') as csvfile:
             greatest_decrease = profit_change
 
         previous_value = current_value
+        
+
+average_change = profit_change_total/(counter-1)
 
 
 print(f"Financial Analysis")
 print(f"-----------------------------")
 print(f"Total Months: {counter}")
 print(f"Total: ${total}")
-#print(f"Average Change: ${average_change}")
+print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
